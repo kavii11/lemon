d802ddeaf69c5ee46481d213fe3cf3df4803b7fc
kavii11 <39086036+kavii11@users.noreply.github.com>
2026-03-31 23:19:15 +0530

    Implement Zustand store for managing blocks

--- app/lib/useBuilder.ts
+++ app/lib/useBuilder.ts
@@ -1,1 +1,26 @@
+"use client";
 
+import { create } from "zustand";
+
+export const useBuilder = create((set) => ({
+  blocks: [],
+
+  addBlock: (type: string) =>
+    set((state: any) => ({
+      blocks: [
+        ...state.blocks,
+        {
+          id: Date.now().toString(),
+          type,
+          props: {},
+        },
+      ],
+    })),
+
+  updateBlock: (id: string, props: any) =>
+    set((state: any) => ({
+      blocks: state.blocks.map((b: any) =>
+        b.id === id ? { ...b, props: { ...b.props, ...props } } : b
+      ),
+    })),
+}));
